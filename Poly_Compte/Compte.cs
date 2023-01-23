6c78a28177d126ae6fe7f0314f05ad1f06a0688c
cda07saba <jeremy.sabadie@2isa.org>
2023-01-23 16:57:33 +0100

    -Création of te méthod.

--- Poly_Compte/Compte.cs
+++ Poly_Compte/Compte.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Poly_Compte
+﻿namespace Poly_Compte
 {
     internal class Compte
     {
+
+
+        private int number { get; set; }
+        private Client holder { get; set; }
+        private DateTime creationDate { get; set; }
+        private float sold { get; set; }
+        public Compte(int number, Client holder, DateTime creationDate, float sold)
+        {
+            this.number = number;
+            this.holder = holder;
+            this.creationDate = creationDate;
+            this.sold = sold;
+            public float addMoney(float mount)
+            {
+                sold = sold + mount;
+                return sold;
+            }
+            public float removeMoney(float mount)
+            {
+                sold = sold - mount;
+                return sold;
+            }
+            public void soldPrint()
+            {
+                Console.WriteLine($"Le solde du compten°: {number} est de:{sold}$");
+            }
+        }
     }
 }
